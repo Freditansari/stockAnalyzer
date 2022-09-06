4028162afad22099e101fe2fc1869cfe61267108
Fredy Yudiawan <Freditansari@users.noreply.github.com>
2022-09-06 10:28:53 +0800

    banana

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.9.5-slim-buster
+RUN apt-get update \
+    && apt-get -y install libpq-dev gcc 
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+COPY . .
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+CMD ["gunicorn","-w", "3", "--thread=4","--bind", "0.0.0.0:5000", "--timeout","600","--log-file", "-", "run:app"]
---
+++ main.py
@@ -0,0 +1,35 @@
+import json
+
+from flask import Flask, render_template, request
+from analyze_stock import analyze_stock
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/analyze/', methods=['GET'])
+def hello():
+    if request.method == 'GET':
+        # print(ticker)
+        ticker = request.args.get('ticker')
+
+        chart_result,\
+             confidence,\
+                 price_chart, \
+                    estimates, \
+                    price_estimates,\
+                         daily_price_chart = analyze_stock(ticker)
+
+        return render_template('chart.html', 
+        chart_result = chart_result, 
+        confidence=confidence, 
+        price_chart= price_chart, 
+        daily_price_chart = daily_price_chart,
+        estimates=estimates, 
+        price_estimates = price_estimates)
+    else:
+        return "Enter ticker"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
